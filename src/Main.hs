33592b824cb1a0d92f44b5a650c05a2735c6b29f
Jonathan Sterling <jonsterling@me.com>
2012-05-05 00:29:31 -0700

    First Commit

---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Text.Lithos
+import Text.ParserCombinators.Parsec (parse)
+
+
+main = do
+  s <- getContents
+  case parse literateDocument "stdin" s of
+    Left err -> putStr ("Error: " ++ show err ++ "\n")
+    Right cs -> print `mapM_`  cs
