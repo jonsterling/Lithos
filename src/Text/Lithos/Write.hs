3cd8ed0d711cc792a5a2ff2f1a5407c7ecc08a42
Jonathan Sterling <jonsterling@me.com>
2012-05-05 03:54:55 -0700

    Style output (WIP)

--- src/Text/Lithos/Write.hs
+++ src/Text/Lithos/Write.hs
@@ -30,8 +30,8 @@ instance WriteHtml Section where
   writeHtml (Section p c) = 
     [shamlet| 
       <tr>
-        <td>^{writeHtml p}
-        <td>^{writeHtml c}
+        <td .prose>^{writeHtml p}
+        <td .code>^{writeHtml c}
     |]
 
 instance WriteHtml Document where
@@ -46,16 +46,9 @@ instance WriteHtml Document where
     |]
 
 
--- This dumb bullshit for Cassius
-data MyRoute = MyRoute
-render _ _ = ""
-
 documentStyle =
   [cassius|
     body
-      font-family: 'Palatino Linotype'
-      font-size: 15px
-      line-height: 22px
       color: #252519
       margin: 0
       padding: 0
@@ -63,17 +56,72 @@ documentStyle =
     p
       margin: 0 0 15px 0
     
-    table td
+    table
+      border-collapse: collapse
+    
+    td
       border: 0
       outline: 0
+      vertical-align: top
 
-    td, th
+    td.prose
       max-width: 450px
       min-width: 450px
       min-height: 5px
       padding: 10px 25px 1 px 50px
       overflow-x: hidden
-      vertical-align: top
       text-align: left
+      font-family: Palatino
+      font-size: 11pt
+      line-height: 1.4em
+
+
+    td.code
+      padding: 14px 15px 16px 25px
+      width: 100%
+      background: #fdf6e3
+      border-left: 1px solid #eee8d5
+
+    pre, tt, code
+      font-size: 11pt
+      line-height: 1.5em
+      font-family: Letter Gothic Std
+      margin: 0
+      padding: 0
+      color: #657b83
+    
+    .kw
+      color: #859900
+    
+    .dt
+      color: #cb4b16
+    .dv 
+      color: #0000FF
+    .bn 
+      color: #0000FF
+    .fl 
+      color: #800080
+    .ch
+      color: #93a1a1
+    .st
+      color: #93a1a1
+    .co
+      color: #93a1a1
+      font-style: italic
+    .ot
       
+    .al
+      color: green
+      font-weight: bold
+    .fu
+      color: #268bd2
+    .re
+    
+    .er
+      color: red
+      font-weight: bold
   |]
+
+-- This dumb bullshit for Cassius
+data MyRoute = MyRoute
+render _ _ = ""
